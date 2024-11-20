823cb0b26453a53047d9b81877fb030d11f5d48d
Kirill Kogan <kkogan@nvidia.com>
2024-11-20 12:17:28 +0200

    Fixed nvidia gpg cert check

--- nv_driver_cuda.sh
+++ nv_driver_cuda.sh
@@ -52,7 +52,7 @@ fi
 
 echo -e "${YELLOW}Verifying nvidia-container-toolkit repository. Please wait...${ENDCOLOR}"
 sleep 1
-if [ -s /etc/apt/sources.list.d/nvidia-container-toolkit.list ]; then
+if [ ! -s /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg ] || [ ! -s /etc/apt/sources.list.d/nvidia-container-toolkit.list ]; then
     curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
     curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
     sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
